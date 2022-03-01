4207c4bafef843546164c037e9ef8ceef8985ecb
Jhon Melendez <jhon.melendez@gmail.com>
2022-03-01 08:32:05 -0500

    Adding testing for token URI

--- test/PlatziPunks.js
+++ test/PlatziPunks.js
@@ -10,15 +10,24 @@ describe("Platzi Punks NFT contract", function () {
     [owner] = await ethers.getSigners();
     PlatziPunks = await ethers.getContractFactory("PlatziPunks");
     PlatziPunksContract = await PlatziPunks.deploy(10);
-  });
-
-  it("Check the owner", async function () {
     const mintReturn = PlatziPunksContract.mint({
       value: ethers.utils.parseEther("0.01"),
     });
+  });
 
+  it("Check the owner", async function () {
     expect(await PlatziPunksContract.ownerOf(0)).to.equal(
       await owner.getAddress()
+    );
+  });
+
+  it("Check token URI", async function () {
+    expect(await PlatziPunksContract.tokenURI(0)).to.equal(
+      "data:application/json;base64,eyAibmFtZSI6ICJQbGF0emlQdW5rcyAj" +
+        "IgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIiwgImRlc2NyaXB0aW" +
+        "9uIjogIlBsYXR6aSBQdW5rcyBhcmUgcmFuZG9taXplZCBBdmF0YWFhcnMgc3Rv" +
+        "cmVkIG9uIGNoYWluIHRvIHRlYWNoIERBcHAgZGV2ZWxvcG1lbnQgb24gUGxhdH" +
+        "ppIiwgImltYWdlIjogIi8vIFRPRE86IENhbGN1bGF0ZSBpbWFnZSBVUkwifQ=="
     );
   });
 });
