c77e36006c1e5f40885c997fe690672dc120f05b
Jhon Melendez <jhon.melendez@gmail.com>
2022-03-01 07:33:17 -0500

    Adding command to display signers and funds

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,15 @@ require("dotenv").config();
 const projectId = process.env.INFURA_PROJECT_ID;
 const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY;
 
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    const balance = await account.getBalance();
+    console.log(`${account.address}: ${balance} Wei`);
+  }
+});
+
 module.exports = {
   solidity: "0.8.4",
   networks: {
